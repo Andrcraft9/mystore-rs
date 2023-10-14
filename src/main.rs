273fe3e27954d57ffd7d2b4e3d5b36f0f4c7a18b
Andrcraft9 <achaplygin99@gmail.com>
2023-10-14 23:21:59 +0300

    Add file manager and text viewer

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use std::{
     default, io,
     os::unix::fs::{self},
     panic,
+    path::Path,
+    path::PathBuf,
     str::FromStr,
 };
 use tui::{
@@ -20,85 +22,166 @@ use tui::{
 
 use console::{Key, Term};
 
-pub struct ListData {
-    pub items: Vec<String>,
-    state: Option<usize>,
+#[derive(Clone)]
+pub enum Action {
+    Back,
+    Root,
 }
 
-impl ListData {
-    pub fn new(items: Vec<String>) -> ListData {
-        ListData {
-            items: items,
-            state: Option::default(),
-        }
+#[derive(Clone)]
+pub enum Entity {
+    TextFile(PathBuf),
+    Folder(PathBuf),
+    Action(Action),
+}
+
+#[derive(Clone)]
+pub enum Respond {
+    Text(String),
+    None,
+}
+
+pub struct FileManager {
+    root: PathBuf,
+    current: PathBuf,
+    entities: Vec<Entity>,
+    selected: Option<usize>,
+}
+
+impl FileManager {
+    fn open_dir<T: AsRef<Path>>(dir: &T) -> Result<Vec<PathBuf>, io::Error> {
+        let items = std::fs::read_dir(dir)?;
+        let file_names: Vec<PathBuf> = items
+            .filter_map(|entry| {
+                let path = entry.ok()?.path();
+                Some(path)
+            })
+            .collect();
+
+        Ok(file_names)
     }
 
-    pub fn get_state(&self) -> Option<usize> {
-        self.state
+    fn create_entities(files: Vec<PathBuf>) -> Vec<Entity> {
+        let mut entities: Vec<Entity> = files
+            .iter()
+            .filter_map(|path| {
+                if path.is_file() {
+                    Some(Entity::TextFile(path.clone()))
+                } else if path.is_dir() {
+                    Some(Entity::Folder(path.clone()))
+                } else {
+                    None
+                }
+            })
+            .collect();
+        entities.push(Entity::Action(Action::Back));
+        entities.push(Entity::Action(Action::Root));
+
+        entities
     }
 
-    pub fn select(&mut self, id: usize) {
-        if (id < self.items.len()) {
-            self.state = Some(id);
-        }
+    fn goto_dir(&mut self, dir: PathBuf) -> Result<(), io::Error> {
+        let files = Self::open_dir(&dir)?;
+        self.entities = Self::create_entities(files);
+        self.selected = None;
+        self.current = dir;
+
+        Ok(())
+    }
+}
+
+impl FileManager {
+    pub fn new(root: &str) -> Result<Self, io::Error> {
+        let files = Self::open_dir(&root)?;
+
+        Ok(Self {
+            current: PathBuf::from(root),
+            root: PathBuf::from(root),
+            entities: Self::create_entities(files),
+            selected: Option::default(),
+        })
+    }
+
+    pub fn get_root(&self) -> PathBuf {
+        self.root.clone()
+    }
+
+    pub fn get_entities(&self) -> Vec<Entity> {
+        self.entities.clone()
+    }
+
+    pub fn get_selected_id(&self) -> Option<usize> {
+        self.selected.clone()
+    }
+
+    pub fn get_selected_entity(&self) -> Option<Entity> {
+        self.selected.map(|id| self.entities[id].clone())
     }
 
     pub fn next(&mut self) {
-        self.state = match self.state {
-            Some(value) => Some((value + 1) % self.items.len()),
-            None => Some(0),
-        };
+        if !self.entities.is_empty() {
+            self.selected = match self.selected {
+                Some(value) => Some((value + 1) % self.entities.len()),
+                None => Some(0),
+            };
+        }
     }
 
     pub fn previous(&mut self) {
-        self.state = match self.state {
-            Some(0) => Some(self.items.len() - 1),
-            Some(value) => Some(value - 1),
-            None => Some(self.items.len() - 1),
-        };
+        if !self.entities.is_empty() {
+            self.selected = match self.selected {
+                Some(0) => Some(self.entities.len() - 1),
+                Some(value) => Some(value - 1),
+                None => Some(self.entities.len() - 1),
+            };
+        }
+    }
+
+    pub fn select(&mut self, id: usize) -> bool {
+        if id < self.entities.len() {
+            self.selected = Some(id);
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn action(&mut self) -> Result<Respond, io::Error> {
+        self.selected
+            .map_or(Ok(Respond::None), |id| match &self.entities[id] {
+                Entity::TextFile(path) => Ok(Respond::Text(std::fs::read_to_string(path)?)),
+                Entity::Folder(path) => {
+                    Self::goto_dir(self, path.clone())?;
+                    Ok(Respond::None)
+                }
+                Entity::Action(act) => {
+                    match act {
+                        Action::Back => {
+                            let parent_path = self.current.parent().map(|path| PathBuf::from(path));
+                            match parent_path {
+                                Some(path) => Self::goto_dir(self, path)?,
+                                None => (),
+                            }
+                        }
+                        Action::Root => Self::goto_dir(self, self.root.clone())?,
+                    }
+                    Ok(Respond::None)
+                }
+            })
     }
 }
 
-/*
-pub struct MenuList<'a> {
-    items: Vec<ListItem<'a>>,
-    state: ListState
+enum Mode {
+    Manager,
+    Viewer,
 }
 
-impl<'a> MenuList<'a> {
-    pub fn new(items: Vec<ListItem<'a>>) -> MenuList<'a> {
-        //let list_items: Vec<ListItem> = items.iter().map(|i| ListItem::new(i.as_ref())).collect();
-        MenuList { items: items, state: ListState::default() }
-    }
-}
-
-impl MenuList<'_> {
-    pub fn add_item(&mut self, item: String) {
-        let list_item = ListItem::new(item);
-        self.items.push(list_item)
-    }
-
-    pub fn draw<B: Backend>(&mut self, frame: &mut Frame<B>) {
-        let text = Text::from("Starting menu");
-
-        let list = List::new(self.items.clone())
-            .block(Block::default()
-            .title("List")
-            .borders(Borders::ALL))
-            .style(Style::default().fg(Color::White))
-            .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
-            .highlight_symbol(">>");
-
-        let margin = Margin{vertical: 1, horizontal: 1};
-        let inner_frame = frame.size().inner(&margin);
-        frame.render_stateful_widget(list, inner_frame, &mut self.state)
-    }
-}
-*/
-
-fn draw_text<B: Backend>(frame: &mut Frame<B>, area: Rect, text_data: &String) {
+fn draw_text<B: Backend>(frame: &mut Frame<B>, area: Rect, text_data: &String, scroll: u16) {
     let text = Text::from(text_data.as_str());
-    let paragraph = Paragraph::new(text);
+    let paragraph = Paragraph::new(text)
+        .block(Block::default().title("Text").borders(Borders::ALL))
+        .wrap(widgets::Wrap { trim: true })
+        .scroll((scroll, 0));
     frame.render_widget(paragraph, area)
 }
 
@@ -107,39 +190,42 @@ fn draw_block<B: Backend>(frame: &mut Frame<B>, area: Rect) {
     frame.render_widget(block, area);
 }
 
-fn draw_list<B: Backend>(frame: &mut Frame<B>, area: Rect, list_data: &ListData) {
+fn draw_list<B: Backend>(
+    frame: &mut Frame<B>,
+    area: Rect,
+    list_data: &Vec<Entity>,
+    list_state: Option<usize>,
+) {
     let items: Vec<ListItem> = list_data
-        .items
         .iter()
-        .map(|i| ListItem::new(i.as_ref()))
+        .map(|entity| match entity {
+            Entity::TextFile(path) => {
+                ListItem::new(path.to_str().map_or("Unknown TextFile", |str| str))
+                    .style(Style::default().fg(Color::White))
+            }
+            Entity::Folder(path) => {
+                ListItem::new(path.to_str().map_or("Unknown Folder", |str| str))
+                    .style(Style::default().fg(Color::Blue))
+            }
+            Entity::Action(act) => match act {
+                Action::Back => ListItem::new("Back").style(Style::default().fg(Color::Red)),
+                Action::Root => ListItem::new("Root").style(Style::default().fg(Color::Red)),
+            },
+        })
         .collect();
     let list = List::new(items)
         .block(Block::default().title("List").borders(Borders::ALL))
-        .style(Style::default().fg(Color::White))
-        .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
-        .highlight_symbol(">>");
+        .highlight_style(Style::default().add_modifier(Modifier::BOLD));
     let mut state = ListState::default();
-    state.select(list_data.get_state());
+    state.select(list_state);
     frame.render_stateful_widget(list, area, &mut state);
 }
 
 fn run_session(terminal: &mut Terminal<CrosstermBackend<io::Stdout>>) -> Result<(), io::Error> {
-    let items = std::fs::read_dir("./")?;
-
-    // Extract the filenames from the directory entries and store them in a vector
-    let file_names: Vec<String> = items
-        .filter_map(|entry| {
-            let path = entry.ok()?.path();
-            if path.is_file() {
-                path.file_name()?.to_str().map(|s| s.to_owned())
-            } else {
-                None
-            }
-        })
-        .collect();
-
-    let mut list_data = ListData::new(file_names);
+    let mut manager = FileManager::new("./")?;
+    let mut mode = Mode::Manager;
     let mut text_data = String::new();
+    let mut text_scroll: u16 = 0;
 
     // Render loop.
     let stdout = Term::stdout();
@@ -158,24 +244,49 @@ fn run_session(terminal: &mut Terminal<CrosstermBackend<io::Stdout>>) -> Result<
                 .direction(tui::layout::Direction::Horizontal)
                 .constraints([Constraint::Percentage(40), Constraint::Percentage(60)])
                 .split(vertical_chunks[1]);
-            draw_list(f, horizontal_chunks[0], &list_data);
-            draw_text(f, horizontal_chunks[1], &text_data);
+            draw_list(
+                f,
+                horizontal_chunks[0],
+                &manager.get_entities(),
+                manager.get_selected_id(),
+            );
+            draw_text(f, horizontal_chunks[1], &text_data, text_scroll);
         })?;
 
         // Handling input.
         let key = stdout.read_key()?;
-        match key {
-            Key::Escape => break Ok(()),
-            Key::ArrowUp => list_data.previous(),
-            Key::ArrowDown => list_data.next(),
-            Key::Enter => text_data = String::from("Hello\nIt\nis\nText\nto\nModify and replace"),
-            Key::Backspace => text_data = String::new(),
-            _ => (),
+        match mode {
+            Mode::Manager => match key {
+                Key::Escape => break Ok(()),
+                Key::ArrowUp => manager.previous(),
+                Key::ArrowDown => manager.next(),
+                Key::Enter => match manager.action()? {
+                    Respond::Text(text) => {
+                        text_data = text;
+                        mode = Mode::Viewer;
+                    }
+                    Respond::None => (),
+                },
+                _ => (),
+            },
+            Mode::Viewer => match key {
+                Key::ArrowUp => {
+                    text_scroll = text_scroll
+                        .checked_sub(1)
+                        .map_or(text_scroll, |scroll| scroll)
+                }
+                Key::ArrowDown => {
+                    text_scroll = text_scroll
+                        .checked_add(1)
+                        .map_or(text_scroll, |scroll| scroll)
+                }
+                _ => {
+                    text_data = String::new();
+                    mode = Mode::Manager
+                }
+            },
         }
     }
-    //let items = vec![String::from_str("First item").unwrap(), String::from_str("Item 2").unwrap()];
-    //let menu_items = items.iter().map(|i| ListItem::new(i.as_ref())).collect();
-    //let mut menu = MenuList::new(menu_items);
 }
 
 fn main() {
